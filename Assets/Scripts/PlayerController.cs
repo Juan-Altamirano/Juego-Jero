e7a826d7bfd08f5b1d6a21f82c60c1b6e7ecd306
Juan-Altamirano <101400097+Juan-Altamirano@users.noreply.github.com>
2022-06-07 21:43:19 -0300

    Avance con el mapa
    Map development

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,6 +17,7 @@ public class PlayerController : MonoBehaviour
         var step = MovSpeed * Time.deltaTime;
 
         //Si apretas W y existe un valor en la variable Transform upTarget, iguala la posicion del casillero target al targetTR
+        //Así con cada dirección
         if (Input.GetKeyDown(KeyCode.W) && upTarget)
         {
             targetTR = upTarget;
@@ -53,14 +54,17 @@ public class PlayerController : MonoBehaviour
 
         }
 
-        //El MoveTowards me pide una posición inicial, una  posición final y una velocidad, en ese orden
+        //El MoveTowards me pide una posición inicial, una posición final y una velocidad, en ese orden
         transform.position = Vector3.MoveTowards(transform.position, targetTR.position, step);
     }
 
     void OnTriggerEnter (Collider other)
     {
+
+        // Si el player se para encima de contra un objeto con el tag "Casillero", agarra los targets de ese casillero y se los guarda en los que se crearon al principio
         if (other.gameObject.tag == "Casillero")
         {
+            //TargetManager es una referencia al script del casillero que estoy "triggereando", para llamarlo y acceder a sus variables públicas, que después voy a guardar en "tm"
             TargetManager tm = other.gameObject.GetComponent<TargetManager>();
             upTarget = tm.upTarget;
             downTarget = tm.downTarget;
