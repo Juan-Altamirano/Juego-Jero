660a6980dab3ddb11220b3ac0c8f85d66e8299c5
Juan-Altamirano <101400097+Juan-Altamirano@users.noreply.github.com>
2022-06-24 08:03:44 -0300

    Obs Romp

---
+++ Assets/Scripts/ObsRomp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsRomp : MonoBehaviour
+{
+
+    // public material Azul
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+        }
+
+        //objeto.GetComponent<Renderer>.material == Azul;
+    }
+}
