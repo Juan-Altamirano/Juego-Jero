e1e16db13685b8c80ca86457dc6841de6d6366de
Juan-Altamirano <101400097+Juan-Altamirano@users.noreply.github.com>
2022-06-25 20:04:39 -0300

    asd

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public float MovSpeed;
     public Transform targetTR;
+
+    public Time tiempo;
 
     public Transform upTarget;
     public Transform downTarget;
@@ -14,6 +17,7 @@ public class PlayerController : MonoBehaviour
 
     public int CoinAmount;
     AudioSource cling;
+    public int movs;
 
     //public GameObject MovibleDer;
     //public GameObject MovibleIzq;
@@ -40,6 +44,7 @@ public class PlayerController : MonoBehaviour
             leftTarget = null;
             rightTarget = null;
 
+            CuentaDeMovs();
         }
 
         //else if (Input.GetKeyDown(KeyCode.W) && MovibleFrente)
@@ -54,6 +59,7 @@ public class PlayerController : MonoBehaviour
             leftTarget = null;
             rightTarget = null;
 
+            CuentaDeMovs();
         }
 
         if (Input.GetKeyDown(KeyCode.A) && leftTarget)
@@ -63,6 +69,7 @@ public class PlayerController : MonoBehaviour
             upTarget = null;
             rightTarget = null;
 
+            CuentaDeMovs();
         }
 
         if (Input.GetKeyDown(KeyCode.D) && rightTarget)
@@ -72,10 +79,25 @@ public class PlayerController : MonoBehaviour
             leftTarget = null;
             upTarget = null;
 
+            CuentaDeMovs();
         }
 
         //El MoveTowards me pide una posición inicial, una posición final y una velocidad, en ese orden
         transform.position = Vector3.MoveTowards(transform.position, targetTR.position, step);
+    }
+
+    //Esta función es medio inútil, solo recordé que para los botones había que seleccionar una función del script que le tirás, y lo confundí con lo que realmente tenía que hacer
+    public void CuentaDeMovs()
+    {
+        if (movs == 0)
+        {
+
+        }
+
+        else
+        {
+            movs--;
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -106,6 +128,8 @@ public class PlayerController : MonoBehaviour
         {
             cling.Play();
         }
+
+        //Por alguna razón si pongo el "cling.Play();" en el script de la moneda, no toma en cuenta la colisión y no hace que desaparezca la coin ni se instancien las "partículas"
     }
 
     //void OnTriggerExit(Collider other) - Idea descartada
