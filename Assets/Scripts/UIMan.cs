176ad73d66ecc31a8dda5b2a45aca1bf296b041b
Juan-Altamirano <101400097+Juan-Altamirano@users.noreply.github.com>
2022-06-26 17:40:21 -0300

    intento de UI y tiempo

---
+++ Assets/Scripts/UIMan.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMan : MonoBehaviour
+{
+    public Text txt_time;
+
+    public string tiemppoo;
+
+    void Start()
+    {
+        tiemppoo = Mathf.FloorToInt(Time.time).ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt_time.text = "Tiempo tardado: " + tiemppoo;
+    }
+}
